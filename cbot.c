185b7093cba7c165471be3997dea37bf9db2dae9
Lars-Ivar Hesselberg Simonsen <larsivarhs@gmail.com>
2011-11-26 17:11:36 +0100

    Completed socket connection

--- cbot.c
+++ cbot.c
@@ -1,7 +1,12 @@
+// for getaddrinfo and the likes
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
 #define BUFFER 512
 
@@ -16,12 +21,12 @@ struct recv_data {
 };
 
 // prototypes
-void die(char* msg);
+void die(char* msg, int err_code);
 void handle_input(struct recv_data in);
 
-void die(char *msg)
+void die(char *msg, int err_code)
 {
-	fprintf(stderr, "%s\n", msg);
+	fprintf(stderr, "%s: %s\n", msg, gai_strerror(err_code));
 	exit(1);
 }
 
@@ -33,17 +38,28 @@ void handle_input(struct recv_data in)
 int main(int argc, char **argv)
 {
 	if (argc != 6) {
-		printf("Usage: %s nick user server port channel\n", argv[0]);
+		printf("Usage: %s nick user host port channel\n", argv[0]);
 		exit(0);
 	}
 	const char *nick = argv[1];
 	const char *user = argv[2];
-	const char *server = argv[3];
-	const int port = atoi(argv[4]);
+	const char *host = argv[3];
+	const char *port = argv[4];
 	const char *channel = argv[5];
 
-	int socket_id;
-	if ((socket_id = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		die("socket could not be opened");
+	int socket_id, err;
+	struct addrinfo hints;
+	struct addrinfo *srv;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((err = getaddrinfo(host, port, &hints, &srv)) != 0)
+		die("getaddrinfo", err);
+	if ((socket_id = socket(srv->ai_family, srv->ai_socktype, 0)) < 0)
+		die("socket", socket_id);
+	if ((err = connect(socket_id, srv->ai_addr, srv->ai_addrlen)) != 0)
+		die("connect", err);
+
 	struct recv_data *irc = malloc(sizeof(*irc));
 }
