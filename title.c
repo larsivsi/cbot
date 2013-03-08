f9bbefd54eca8b601d52469eabe4423e2a6f850e
Lars-Ivar Hesselberg Simonsen <larsivarhs@gmail.com>
2013-03-08 14:55:39 +0100

    Limit pcre to only search the received bytes rather than the entire buffer

--- title.c
+++ title.c
@@ -62,7 +62,7 @@ void get_title_from_url(struct recv_data *in, const char *url)
 	}
 
 	int titles[30];
-	int titlecount = pcre_exec(patterns->html_title, 0, http_buffer, HTTP_BUFFER, 0, 0, titles, 30);
+	int titlecount = pcre_exec(patterns->html_title, 0, http_buffer, http_buffer_pos, 0, 0, titles, 30);
 	char title[BUFFER_SIZE];
 	if (titlecount > 0) {
 		pcre_copy_substring(http_buffer, titles, titlecount, 1, title, BUFFER_SIZE);
