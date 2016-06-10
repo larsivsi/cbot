3dca4afd810f120e48ea2b37ab49dae50734db5b
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-06-10 22:10:04 +0200

    remove dead crypto code

--- crypto.h
+++
@@ -1,8 +0,0 @@
-#ifndef CRYPTO_H
-#define CRYPTO_H
-
-const char *encrypt(const char *string);
-const char *decrypt(const char *string);
-
-/* vim: set ts=8 sw=8 tw=0 noexpandtab cindent softtabstop=8 :*/
-#endif//CRYPTO_H
